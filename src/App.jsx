a8d8fdb557e3f8b9c31ce9340a7aede838815113
Phumlani <mdzeshh@gmail.com>
2026-03-21 13:17:10 +0200

    Saved

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ClipLoader } from 'react-spinners'
 import './App.css'
 import CompanyLogo from './assets/Img/logo.jpg'
 
-const API_BASE_URL = 'http://10.150.51.51:5000'
+const API_BASE_URL = 'https://phone-bool-eswatini.onrender.com/'
 
 const STORAGE_KEYS = {
   theme: 'sacco_portal_theme',
